6ba9ffb97fdd5832cb36cb6d0cb4c2257c04e9ac
nicmackenzie <nicmackenzie@live.com>
2023-07-11 21:27:37 +0300

    completed assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name text,
+    age INT,
+    sex text,
+    color text,
+    temperament text,
+    alive boolean
+)
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES("Mr. Chocolate",6,'M','white','something',1),
+("Rowdy",4,'F','white','something',1),
+("Tabitha",4,'F','brown','something',0),
+("Sergeant Brown",2,'M','white','something',1),
+("Melissa",1,'F','cream','something',1),
+("Grinch",3,'M','brown','something',1),
+("Wendy",5,'M','black','something',0),
+(NULL,2,'M','black','something',0);
+
